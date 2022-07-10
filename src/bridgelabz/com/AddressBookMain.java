e2d3bbc668c1c5cd435a8d2ccad091be3d1f3506
Govind maithil <govindmaithilrtn@gmail.com>
2022-07-10 14:02:38 +0530

    added Edit contact in address book program

--- src/bridgelabz/com/AddressBookMain.java
+++ src/bridgelabz/com/AddressBookMain.java
@@ -7,17 +7,21 @@ public class AddressBookMain {
 public static void main(String[] args) {
 		
 		System.out.println("How many Contacts you want to add:  ");
-		
+
 		Scanner scanner = new Scanner(System.in);
-		int numOfContact = scanner.nextInt();
-		addContacts(scanner,numOfContact );
+		
+		int numOfContact = scanner.nextInt();	
+		Contacts contact[] = new Contacts[numOfContact];
+		addContacts(scanner,numOfContact,contact );
+		
+		String email = scanner.next();
+		System.out.println("Enter a person email to edit the contact");
+		editContacts(scanner, contact, email);
 		}
 	
-	private static void addContacts(Scanner scanner , int numOfContact) {
-		
-		
-		Contacts contact[] = new Contacts[numOfContact];
-		
+	private static void addContacts(Scanner scanner , int numOfContact , Contacts contact[]) 
+	{
+		int id;
 		String firstName;
 		String lastName;
 		String address;
@@ -27,6 +31,9 @@ public static void main(String[] args) {
 		String phoneNumber;
 		
 		for (int i=0; i<contact.length;i++ ) {
+			
+		    System.out.println("Enter id ");
+		    id = scanner.nextInt();
 			
 			System.out.println("Enter First name: ");
 			firstName = scanner.next();
@@ -59,7 +66,56 @@ public static void main(String[] args) {
 		}
 		
 	}
+	private static void editContacts(Scanner scanner , Contacts[] contact, String email) 
+	{
+		
+		String firstName;
+		String lastName;
+		String address;
+		String city;
+		int zip;
+		String phoneNumber;
+		
+		for (int i=0; i<contact.length;i++ ) {
+			
+			if (contact[i].getEmail().equalsIgnoreCase(email)) {
+			
+			System.out.println("Enter First name: ");
+			firstName = scanner.next();
+			contact[i].setFirstName(firstName);
+			
+			System.out.println("Enter Last name: ");
+			lastName = scanner.next();
+			contact[i].setLirstName(lastName);
+			
+			System.out.println("Enter your Address: ");
+			address = scanner.next();
+			contact[i].setAddress(address);
+			
+			System.out.println("Enter Your City: ");
+			city = scanner.next();
+			contact[i].setCity(city);
+			
+			System.out.println("Enter Zip code: ");
+			zip = scanner.nextInt();
+			contact[i].setZip(zip);
 
-	
-	
+			System.out.println("Enter Your Email");
+			email = scanner.next();
+			contact[i].setEmail(email);
+
+			System.out.println("Enter Your Number:");
+			phoneNumber = scanner.next();
+			contact[i].setPhoneNumber(phoneNumber);
+			
+			}
+
+		}
+		
+		for (int i = 0 ; i<contact.length; i++) 
+		{
+			System.out.println(contact[i]);
+		}
+		
+	}
 }
