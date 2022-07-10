bdda139570fc6b5f59a8ec05ca322767b39b4e0d
Govind maithil <govindmaithilrtn@gmail.com>
2022-07-10 12:22:10 +0530

    Added Add contact in Address book

--- src/bridgelabz/com/AddressBookMain.java
+++ src/bridgelabz/com/AddressBookMain.java
@@ -1,10 +1,65 @@
 package bridgelabz.com;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
 	
 public static void main(String[] args) {
 		
-		System.out.println("Welcome to Address book System");
+		System.out.println("How many Contacts you want to add:  ");
+		
+		Scanner scanner = new Scanner(System.in);
+		int numOfContact = scanner.nextInt();
+		addContacts(scanner,numOfContact );
+		}
+	
+	private static void addContacts(Scanner scanner , int numOfContact) {
+		
+		
+		Contacts contact[] = new Contacts[numOfContact];
+		
+		String firstName;
+		String lastName;
+		String address;
+		String city;
+		int zip;
+		String email;
+		String phoneNumber;
+		
+		for (int i=0; i<contact.length;i++ ) {
+			
+			System.out.println("Enter First name: ");
+			firstName = scanner.next();
+			
+			System.out.println("Enter Last name: ");
+			lastName = scanner.next();
+			
+			System.out.println("Enter your Address: ");
+			address = scanner.next();
+			
+			System.out.println("Enter Your City: ");
+			city = scanner.next();
+			
+			System.out.println("Enter Zip code: ");
+			zip = scanner.nextInt();
+
+			System.out.println("Enter Your Email");
+			email = scanner.next();
+
+			System.out.println("Enter Your Number:");
+			phoneNumber = scanner.next();
+			
+			contact[i] = new Contacts(firstName, lastName , address, city, zip, email ,phoneNumber);
+
+		}
+		
+		for (int i = 0 ; i<contact.length; i++) 
+		{
+			System.out.println(contact[i]);
+		}
+		
 	}
 
+	
+	
 }
